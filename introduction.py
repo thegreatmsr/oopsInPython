c47356650b81dac65db7d95e8bf79e7d7e01802b
Manav S. Rajput <theexplainer007@gmail.com>
2025-03-27 11:07:46 +0530

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,14 @@
+# POP approcahes does'nt gurantee the data security in the programming So that is why we use the OOPS concept in the programming
+def sum(a,b):
+    c=a+b
+    print(c)
+sum(5,4)
+# Class can be consider as the instructor in which it will have the following teachers name jiya and payal mainly they will consider as the obect of the instructor (Class)
+#Class is the blueprint to create a objects just a like a blueprint is used to create the houses
+# Class Name can be caled using the PascalCase
+class InstructorInfo:
+    pass
+
+
+instructor_1= InstructorInfo()
+
